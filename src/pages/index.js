ebe7e72cd9e66e7537f9672eac0049fa64ac08b7
Akash <akash@appycodes.com>
2025-05-29 18:29:35 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,30 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import LayoutAnimation from "@/components/LayoutAnimation";
+import ScrollTriggered from "@/components/ScrollTriggered";
+import ScrollLinked from "@/components/ScrolledLinked";
+import SharedLayoutAnimation from "@/components/SharedLayoutAnimation";
+import * as motion from "motion/react-client";
 
 export default function Home() {
   return (
-    
+    <div className="flex gap-6 item-center justify-center w-full py-12 max-w-96 flex-wrap">
+      {Array.from({ length: 10 }, (_, i) => i + 1).map((a) => (
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 0.5 }}
+          viewport={{ once: false }}
+          style={box}
+          key={a}
+        />
+      ))}
+    </div>
   );
 }
+
+const box = {
+  width: 300,
+  height: 300,
+  backgroundColor: "#f5f5f5",
+  borderRadius: 5,
+  margin: "auto",
+};
