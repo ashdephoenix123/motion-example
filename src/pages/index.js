9a583b85c64cdda71f1ab154d512fd2dba0d9472
Akash <akash@appycodes.com>
2025-05-30 15:22:37 +0530

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,12 @@ import SharedLayoutAnimation from "@/components/SharedLayoutAnimation";
 import * as motion from "motion/react-client";
 
 export default function Home() {
-  return (
-    <div className="flex gap-6 item-center justify-center w-full py-12 max-w-96 flex-wrap">
-      {Array.from({ length: 10 }, (_, i) => i + 1).map((a) => (
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          transition={{ duration: 0.5 }}
-          viewport={{ once: false }}
-          style={box}
-          key={a}
-        />
-      ))}
-    </div>
-  );
+  return <motion.div style={box} drag />;
 }
 
 const box = {
-  width: 300,
-  height: 300,
+  width: 100,
+  height: 100,
   backgroundColor: "#f5f5f5",
   borderRadius: 5,
   margin: "auto",
